b59670acb74e545c5820fc72f9464e311da1f80d
Sheikh, Haroon <haroon.sheikh@bskyb.com>
2016-03-15 00:02:08 +0000

    Renaming step definitions file

--- src/test/java/com/sitture/mvn/cucumber/appium/HamburgerDef.java
+++ src/test/java/com/sitture/mvn/cucumber/appium/HamburgerDef.java
@@ -4,7 +4,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class AddContactStepDefs {
+public class HamburgerDef {
 
 	@Given("^I launch the app$")
 	public void I_launch_the_app() throws Throwable {
